72cde74acad5d80a01cc5de168122e8bc192c537
caleboppong <maroppcal@hotmail.it>
2026-04-07 20:22:13 -0400

    Setup GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/shoppingreact/',
+    base: "/https://github.com/caleboppong/e-plantShopping",
   plugins: [react()],
-  server: {
-    host: '0.0.0.0',
-    port: 8888,
-  },
 })
