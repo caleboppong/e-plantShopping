309b1f3214e767c46271e7d90da8fa81e5312ca6
caleboppong <maroppcal@hotmail.it>
2026-04-07 20:00:35 -0400

    Verify global Redux store setup in main.jsx

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: "/shoppingreact",
+  base: '/shoppingreact/',
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    port: 8888,
+  },
 })
