b0b447d2f5488aa47fc2f5979f58b01faaef3367
caleboppong <maroppcal@hotmail.it>
2026-04-07 20:43:38 -0400

    Fix GitHub Pages blank page issue

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -279,7 +279,7 @@ function ProductList({ onHomeClick }) {
                             src="https://cdn.pixabay.com/photo/2020/08/05/13/12/eco-5465432_1280.png"
                             alt=""
                         />
-                        <a href="/" onClick={handleHomeClick}>
+                        <a href="#" onClick={handleHomeClick}>
                             <div>
                                 <h3 style={{ color: 'white' }}>Paradise Nursery</h3>
                                 <i style={{ color: 'white' }}>Where Green Meets Serenity</i>
